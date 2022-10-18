74a31cd8191610f3267de7715374b1fb76c1f890
Nota <100799245+notacja@users.noreply.github.com>
2022-10-18 23:05:57 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,8 +1,14 @@
 function copyToClipboard(element) {
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val($(element).text()).select();
-    document.execCommand("copy");
-    $temp.remove();
-  }
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
   
+  //const text =  document.querySelector('.col span')
+  //const popup = document.querySelector('.popup');
+
+  //text.addEventListener('click', ()=>{
+   // popup.classList.add('active');
+ // })
